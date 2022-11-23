91d72e41257a05e3b2fcec2e69fb638691f73b4f
zhoujs <715842648@qq.com>
2022-11-24 02:35:22 +0800

    comp: categorySelector

---
+++ src/components/CategorySelector.vue
@@ -0,0 +1,57 @@
+<template>
+  <div @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
+    <div id="category" class="item">Category</div>
+    <div v-if="categoriesVisible" class="categories">
+      <a class="categoryLink d-block col-4" v-for="(item, index) in categories" :key="index">{{ item }}</a>
+    </div>
+  </div>
+</template>
+<script>
+// 选取电影类别 跳转类别页面
+import { ref } from 'vue';
+export default {
+  name: "CategorySelector",
+  setup() {
+    const categories = ref(["All", "Action", "Biography", "Crime", "Family", "Horror",
+      "Romance", "Sports", "War", "Adventure", "Comedy", "Documentary", "Fantasy", "Thriller",
+      "Animation", "Costume", "Drama", "History", "Musical", "Psychological"])
+    const categoriesVisible = ref(false)
+
+    const onMouseEnter = () => {
+      categoriesVisible.value = true
+      document.getElementById('category').style.backgroundColor = 'rgb(255, 248, 248)'
+      document.getElementById('category').style.color = 'black'
+    }
+    const onMouseLeave = () => {
+      categoriesVisible.value = false
+      document.getElementById('category').style.backgroundColor = '#333'
+      document.getElementById('category').style.color = 'white'
+    }
+
+    return {
+      categories,
+      categoriesVisible,
+      onMouseEnter,
+      onMouseLeave
+    }
+  }
+}
+</script>
+<style>
+.categories {
+  display: flex;
+  max-width: 350px;
+  padding: 12px;
+  position: absolute;
+  flex-wrap: wrap;
+  background-color: rgb(255, 248, 248);
+  z-index: 999;
+}
+
+.categoryLink {
+  text-decoration: none;
+  color: black;
+  font-size: 14px;
+  text-align: left;
+}
+</style>
