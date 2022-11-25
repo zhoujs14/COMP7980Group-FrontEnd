32a688ed599df437c6c1596cfec9866a937af0f2
zhoujs <715842648@qq.com>
2022-11-25 20:00:21 +0800

    modals for sign in & sign up

--- src/components/user/NavBar.vue
+++ src/components/user/NavBar.vue
@@ -14,31 +14,73 @@
 
   <!--Sign In Modal -->
   <div class="modal fade" id="signInModal" tabindex="-1" aria-labelledby="label" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          <form>
+          <form class="m-4">
             <div class="mb-3">
-              <label class="col-2" for="AccountField">Account</label>
-              <input id="AccountField" type="text" class="form-control" />
+              <label class="col-2" for="AccountField1">Account</label>
+              <input id="AccountField1" type="text" class="form-control" v-model="signInForm.account" />
             </div>
             <div class="mb-3">
-              <label class="col-2" for="passwordField">Password</label>
-              <input id="passwordField" type="password" class="form-control" />
+              <label class="col-2" for="passwordField1">Password</label>
+              <input id="passwordField1" type="password" class="form-control" v-model="signInForm.password" />
             </div>
           </form>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn">Sign up</button>
-          <button type="button" class="btn btn-primary">Sign In</button>
+          <button type="button" class="btn" data-bs-target="#signUpModal" data-bs-toggle="modal">Sign up</button>
+          <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="onSignIn">Sign In</button>
         </div>
       </div>
     </div>
-
   </div>
+
+  <!--Sign Up Modal -->
+  <div class="modal fade" id="signUpModal" tabindex="-1" aria-labelledby="label" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <span>Register</span>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form action="" class="m-4">
+            <div class="mb-3">
+              <label class="col-2" for="AccountField2">Account</label>
+              <input necessary id="AccountField2" type="text" class="form-control" v-model="signUpForm.account" />
+            </div>
+            <div class="mb-3">
+              <label class="col-2" for="passwordField2">Password</label>
+              <input id="passwordField2" type="password" class="form-control" v-model="signUpForm.password" />
+            </div>
+            <div class="mb-3">
+              <label class="col-2" for="nickNameField">NickName</label>
+              <input id="nickNameField" type="text" class="form-control" v-model="signUpForm.nickName" />
+            </div>
+            <div class="mb-3">
+              <label class="col-2" for="emailField">Email</label>
+              <input id="emailField" type="email" class="form-control" v-model="signUpForm.email" />
+            </div>
+            <div class="mb-3">
+              <label class="col-2" for="ageField">Age</label>
+              <input id="ageField" type="number" class="form-control" :min="0" :max="200" v-model="signUpForm.age" />
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary2" data-bs-target="#signUpModal" data-bs-toggle="modal"
+            @click="onSignUp">Sign
+            up</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
 </template>
 
 <script>
@@ -51,7 +93,23 @@ export default {
   name: "NavBar",
   components: { CategorySelector },
   setup() {
-    return {}
+    const signInForm = ref({})
+    const signUpForm = ref({})
+
+    const onSignIn = () => {
+      alert('sign in' + JSON.stringify(signInForm.value))
+    }
+
+    const onSignUp = () => {
+      alert('sign up' + JSON.stringify(signUpForm.value))
+    }
+
+    return {
+      signInForm,
+      signUpForm,
+      onSignIn,
+      onSignUp
+    }
   }
 }
 </script>
@@ -88,5 +146,10 @@ form>div {
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
+}
+
+label {
+  text-align: right;
+  margin-right: 8px;
 }
 </style>
--- vue.congig.js
+++ vue.congig.js
@@ -1,3 +1,27 @@
 module.exports = {
-  publicPath: './'
+  publicPath: './',
+  outputDir: "dist",
+  assetsDir: "static",
+  indexPath: 'index.html',
+  devServer: {
+    overlay: {
+      warnings: false,
+      errors: false
+    },
+    // 设置主机地址
+    host: 'localhost',
+    // 设置默认端口
+    port: 8080,
+    // 设置代理
+    proxy: {
+      '^/api/.*': {
+        // 目标 API 地址
+        target: 'http://localhost:3000', // 接口的域名
+        // 如果要代理 websockets
+        ws: false,
+        // 将主机标头的原点更改为目标URL
+        changeOrigin: true
+      }
+    }
+  }
 }
